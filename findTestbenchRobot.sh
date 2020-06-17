f5109cf877634918ad1bfe186f33c615fca5041f
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-06-17 08:01:31 -0600

    Added script to search the network for testbenches, then search testbenches for robots

---
+++ findTestbenchRobot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e # Exit on first error
+
+nmapResults=$(sudo nmap -sn 10.0.5.188/22 | grep -i 'testbench\|deploy\|metalXO')
+
+echo "$nmapResults"
+echo ''
+
+checkRobot="timeout 2 sshpass -p 'sarcos' ssh sarcos@192.168.113.2 'hostname' || echo no robot"
+
+readarray testBenchList <<<"${nmapResults}"
+for var in "${testBenchList[@]}"; do
+
+	# Isolate the ip and domain name
+	ip="$(echo "${var}" | cut -d' ' -f6 | tr -d '()')"
+    domain="$(echo "${var}" | cut -d' ' -f5)"
+    echo "${domain} (${ip})"
+
+    timeout 3 sshpass -p 'sarcos' ssh "sarcos@${ip}" "${checkRobot}" || echo 'cant enter as sarcos'
+
+    echo ''
+
+done
+
