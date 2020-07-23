32556e49b87f6dd270a5c8512ac011cb8b48fa5e
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-07-23 12:12:52 -0600

    Created a script to check file extension and use correct linter

---
+++ lint.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+
+function lintHelper()
+{
+    LINTER="${1}"
+    LINTER_ARGS="${2}"
+    FILENAME="${3}"
+
+    echo "================================================="
+    echo "Linting '${FILENAME}' with '${LINTER} ${LINTER_ARGS}'."
+    echo ''
+
+    # Run linter. Print error message and exit if file doesn't pass
+    "${LINTER}" "${ARGS}" "${FILENAME}" || { echo "'${FILENAME}' did not pass linter."; exit 1; }
+    echo ''
+}
+
+
+if [[ "$#" -eq '0' ]]; then
+    echo 'This script lints files based on the file extension.'
+    echo 'Usage:'
+    echo '    lint.sh file1, file2, ... fileN'
+    exit 0
+fi
+
+
+# Loop over input files
+for FILENAME in "$@"
+do
+
+    LINTER_ARGS='' # Reset to no extra args
+
+    case "${FILENAME}" in
+
+        *'.cpp' | *'.c' | *'.h')
+            LINTER='python3 -m cpplint'
+            ;;
+
+        *'.py')
+            LINTER='python3 -m flake8'
+            LINTER_ARGS='--max-line-length 120'
+            ;;
+
+        *'.sh')
+            LINTER=shellcheck
+            ;;
+
+        *'.yml' | *'.ymal')
+            LINTER=yamllint
+            ;;
+
+        *'.xml')
+            LINTER=xmllint
+            ;;
+
+        *) # default
+            echo "Default"
+            ;;
+
+    esac
+
+    lintHelper "${LINTER}" "${ARGS}" "${fileName}"
+
+done
