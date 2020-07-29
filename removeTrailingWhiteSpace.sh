51cbd39bf01c63efa9e47d8b8a2726618687e88b
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-07-29 11:25:03 -0600

    fix to pass shellcheck

--- removeTrailingWhiteSpace.sh
+++ removeTrailingWhiteSpace.sh
@@ -3,12 +3,12 @@
 case "${OSTYPE}" in
     'linux-gnu'*)
         # Gnu Sed is the default sed
-        /bin/cat "$1" | sed -e "s/\s*$//"
+        sed -e 's/\s*$//' "$1"
         ;;
 
     *)
         # Any other operating system, try 'gsed'
-        /bin/cat "$1" | gsed -e "s/\s*$//"
+        gsed -e 's/\s*$//' "$1"
         ;;
 esac
 
