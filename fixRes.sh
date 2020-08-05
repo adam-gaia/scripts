e7ea33d66e71b418e914c5046c6e4cf129aa5453
Adam Gaia <36975781+adam-gaia@users.noreply.github.com>
2020-08-05 16:20:24 -0600

    updated fix res script

--- fixRes.sh
+++ fixRes.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+# Old settings - 1 Acer 23inch display and built in display
+#xrandr -q
+#xrandr --addmode eDP-1 1920x1080
+#xrandr --output  eDP-1 --mode 1920x1080
+#xrandr --addmode DP-3  1920x1080
+#xrandr --output  DP-3  --mode 1920x1080
+#arandr
+
+# New settings - 2 Acer displays and built in display
 xrandr -q
 xrandr --addmode eDP-1 1920x1080
 xrandr --output  eDP-1 --mode 1920x1080
-xrandr --addmode DP-3  1920x1080
-xrandr --output  DP-3  --mode 1920x1080
+xrandr --addmode DP-1-1 1920x1080
+xrandr --output  DP-1-1 --mode 1920x1080
+xrandr --addmode DP-1-3 1920x1080
+xrandr --output  DP-1-3 --mode 1920x1080
 arandr
